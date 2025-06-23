a25afdbf0ecec600868b32e9aa6c0111d28d640f
Gregory Dort <gregory.dort@gmail.com>
2025-06-23 15:04:56 -0400

    Created Starting Routes and API Endpoints

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     const [message, setMessage] = useState('');
 
     const handleLogin = async (e) => {
-        e,preventDefault();
+        e.preventDefault();
 
         try {
             const response = await fetch('http://127.0.0.1:8000/login/', {
